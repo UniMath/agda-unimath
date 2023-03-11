5408eb063f4946b74aaf23144bc80eb1a6d3a3a3
Fredrik Bakke <fredrbak@gmail.com>
2023-03-11 17:10:03 +0100

    respect extra contents of index files

--- hooks/generate_indexes.py
+++ hooks/generate_indexes.py
@@ -2,71 +2,79 @@
 # Run this script:
 # $ python3 hooks/generate_indexes.py fileName.lagda.md
 
-# make a dict :
-#   dict = { namespace : [files] }
-# iterate src folder
-#   get the dirs
-#   for d in dirs:
-#      fs = collect all the files
-#      for f in fs:
-#        dict
-
-
-from collections import defaultdict
 import os
 import pathlib
-import sys
 import utils
 
 status = 0
 root = "src"
-
 
 def get_subdirectories(startpath):
     for root, dirs, files in os.walk(startpath):
         for d in dirs:
             yield d
 
+
 def get_files(path):
     return os.listdir(path)
 
+
 def generate_title(namespace):
-    return namespace.capitalize().replace("-"," ")
+    return "# " + namespace.capitalize().replace("-", " ") + "\n"
+
 
 def generate_imports(namespace):
     namespace_path = os.path.join(root, namespace)
     namespace_files = filter(lambda f: utils.isAgdaFile(
-        pathlib.Path(os.path.join(namespace_path, f)))
-          , get_files(namespace_path))
+        pathlib.Path(os.path.join(namespace_path, f))), get_files(namespace_path))
 
-    # 
     return "\n".join(sorted("open import " + namespace + "." + module_file[:module_file.index(".lagda.md")] + " public" for module_file in namespace_files))
 
-template = \
-'''# {title}
 
-```agda
+agda_block_template = \
+'''```agda
 module {namespace} where
 
 {imports}
 ```
 '''
 
-def generate_index(namespace):
 
-    title = generate_title(namespace)
-    imports = generate_imports(namespace)
+def generate_agda_block(namespace):
+    return agda_block_template.format(namespace=namespace, imports=generate_imports(namespace))
 
-    return template.format(title=title, namespace=namespace, imports=imports)
 
-    
 if __name__ == "__main__":
     namespaces = get_subdirectories(root)
     for namespace in namespaces:
-        with open(os.path.join(root, namespace)+".lagda.md", "w") as namespace_file:
-          namespace_file.write(generate_index(namespace))
+        with open(os.path.join(root, namespace) + ".lagda.md", "r+") as namespace_file:
 
-    # with open(fpath, 'r', encoding='UTF-8') as file:
-    #     contents = file.read()
+            contents = namespace_file.read()
 
-  # sys.exit(status)
+            title_index = contents.find("#")
+            if title_index > 0:
+                print(
+                    f"Warning! Namespace file {namespace_file} has title after first line.")
+            elif title_index == -1:  # Missing title. Generate it
+                contents = generate_title(namespace) + contents
+
+            agda_block_start = contents.find("```agda")
+            if agda_block_start == -1:
+                # Must add agda block
+                # Add at the end of the file
+                contents = contents + "\n" + generate_agda_block(namespace)
+            else:
+                agda_block_end = contents.find(
+                    "```\n", agda_block_start + len("```agda"))
+                if agda_block_end == -1:
+                    # An agda block is opened but not closed.
+                    # This is an error, but we can fix it
+                    print(f"Warning! agda-block was opened but not closed in {namespace_file}.")
+                    contents = contents[:agda_block_start] + \
+                        generate_agda_block(namespace)
+                else:
+                    contents = contents[:agda_block_start] + generate_agda_block(
+                        namespace) + contents[agda_block_end+len("```\n"):]
+                    
+            with open(os.path.join(root, namespace) + ".lagda.md", "w") as f:
+              f.write(contents)
