e7cf783870fee70d029b5dfe1194eb9cabbc37ae
Fredrik Bakke <fredrbak@gmail.com>
2023-03-12 15:58:30 +0100

    fix module generator bug

--- hooks/generate_namespace_index_modules.py
+++ hooks/generate_namespace_index_modules.py
@@ -3,6 +3,7 @@
 # $ python3 hooks/generate_namespace_index_modules.py
 
 import os
+import sys
 import pathlib
 import utils
 
@@ -46,40 +47,43 @@ def generate_agda_block(namespace):
 
 
 if __name__ == "__main__":
-    namespaces = get_subdirectories(root)
-    for namespace in namespaces:
+    for namespace in get_subdirectories(root):
         namespace_filename = os.path.join(root, namespace) + ".lagda.md"
         with open(namespace_filename, "a+") as namespace_file:
             pass
-        with open(namespace_filename, "r") as namespace_file:
-
+        with open(namespace_filename, "r+") as namespace_file:
             contents = namespace_file.read()
 
-            title_index = contents.find("# ")
-            if title_index > 0:
+        oldcontents = contents
+
+        title_index = contents.find("# ")
+        if title_index > 0:
+            print(
+                f"Warning! Namespace file {namespace_filename} has title after first line.")
+        elif title_index == -1:  # Missing title. Generate it
+            contents = generate_title(namespace) + contents
+
+        agda_block_start = contents.rfind("```agda\n")
+        if agda_block_start == -1:
+            # Must add agda block
+            # Add at the end of the file
+            contents += "\n" + generate_agda_block(namespace)
+        else:
+            agda_block_end = contents.find(
+                "\n```\n", agda_block_start + len("```agda\n"))
+            if agda_block_end == -1:
+                # An agda block is opened but not closed.
+                # This is an error, but we can fix it
                 print(
-                    f"Warning! Namespace file {namespace_filename} has title after first line.")
-            elif title_index == -1:  # Missing title. Generate it
-                contents = generate_title(namespace) + contents
-
-            agda_block_start = contents.rfind("```agda\n")
-            if agda_block_start == -1:
-                # Must add agda block
-                # Add at the end of the file
-                contents = contents + "\n" + generate_agda_block(namespace)
+                    f"Warning! agda-block was opened but not closed in {namespace_filename}.")
+                contents = contents[:agda_block_start] + \
+                    generate_agda_block(namespace)
             else:
-                agda_block_end = contents.find(
-                    "\n```\n", agda_block_start + len("```agda\n"))
-                if agda_block_end == -1:
-                    # An agda block is opened but not closed.
-                    # This is an error, but we can fix it
-                    print(
-                        f"Warning! agda-block was opened but not closed in {namespace_filename}.")
-                    contents = contents[:agda_block_start] + \
-                        generate_agda_block(namespace)
-                else:
-                    contents = contents[:agda_block_start] + generate_agda_block(
-                        namespace) + contents[agda_block_end+len("\n```\n"):]
+                contents = contents[:agda_block_start] + generate_agda_block(
+                    namespace) + contents[agda_block_end+len("\n```\n"):]
 
-            with open(os.path.join(root, namespace) + ".lagda.md", "w") as f:
-                f.write(contents)
+        if (oldcontents != contents):
+          status |= 1
+          with open(namespace_filename, "w") as f:
+              f.write(contents)
+    sys.exit(status)
