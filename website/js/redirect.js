03c8de076471510476559c42793eea84dffb2e61
Fredrik Bakke <fredrbak@gmail.com>
2024-03-01 17:05:01 +0100

    remove redirection

--- website/js/redirect.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-  if (window.location.href.includes("/redirect/goatcounter")) {
-    window.location.href = 'https://agda-unimath.goatcounter.com/count';
-  }
-});
